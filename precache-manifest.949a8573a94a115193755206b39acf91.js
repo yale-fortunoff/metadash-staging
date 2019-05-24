cf72ca13b7151c2b0cb39545804574f84fb9c559
Jake Kara <jake@jakekara.com>
2019-05-24 16:51:11 -0400

    Updates

---
+++ precache-manifest.949a8573a94a115193755206b39acf91.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d89e9a8ceb8f1e1cdb2a5fa1b6f5fcc",
+    "url": "/fortunoff-metadash/index.html"
+  },
+  {
+    "revision": "a942f3a98d2bfac90b31",
+    "url": "/fortunoff-metadash/static/css/main.d0a7bb59.chunk.css"
+  },
+  {
+    "revision": "0f672da1a709be371d9d",
+    "url": "/fortunoff-metadash/static/js/2.7b69c468.chunk.js"
+  },
+  {
+    "revision": "a942f3a98d2bfac90b31",
+    "url": "/fortunoff-metadash/static/js/main.0873cec0.chunk.js"
+  },
+  {
+    "revision": "74ded0f82e8e1bfa3b30",
+    "url": "/fortunoff-metadash/static/js/runtime~main.0a15b005.js"
+  },
+  {
+    "revision": "0f06653d4b40a6394ea7837a94734ffe",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Bold.0f06653d.ttf"
+  },
+  {
+    "revision": "62d4d7d369292a9bf23762465ec6d704",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Bold.62d4d7d3.otf"
+  },
+  {
+    "revision": "410504d49238e955ba7dc23a7f963021",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Reg.410504d4.otf"
+  },
+  {
+    "revision": "7aa23808a8f551d365d52e8a8f4297e7",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Reg.7aa23808.ttf"
+  },
+  {
+    "revision": "2813c7a70fa32e7f471109dddbd3a7d0",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Sbold.2813c7a7.ttf"
+  },
+  {
+    "revision": "6a386899746222073dd64c5f74d1a69d",
+    "url": "/fortunoff-metadash/static/media/ProximaNova-Sbold.6a386899.otf"
+  },
+  {
+    "revision": "5f106b04a24dff00e969d18581d23cc1",
+    "url": "/fortunoff-metadash/static/media/YaleNew-Bold.5f106b04.otf"
+  },
+  {
+    "revision": "b61ece380b63d0190e15d487a2211b5a",
+    "url": "/fortunoff-metadash/static/media/YaleNew-Roman.b61ece38.otf"
+  },
+  {
+    "revision": "1d4d325f152154600232fb1184450ab0",
+    "url": "/fortunoff-metadash/static/media/spy-glass.1d4d325f.svg"
+  }
+]);
