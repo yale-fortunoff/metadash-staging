47646b7a5cfa1f4196c5c22768880b3c8cece248
Jake Kara <jake@jakekara.com>
2019-10-23 10:43:52 -0400

    Updates

---
+++ precache-manifest.d3f87067c5a4f7faa5c30c6f4046baef.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06e98edb00e4677f116d9911ef951cfe",
+    "url": "/fortunoff-metadash/index.html"
+  },
+  {
+    "revision": "4ba2ce8afd05695772ee",
+    "url": "/fortunoff-metadash/static/css/main.6e1a2ffb.chunk.css"
+  },
+  {
+    "revision": "6977d0a68d0473292c39",
+    "url": "/fortunoff-metadash/static/js/2.3648ce9c.chunk.js"
+  },
+  {
+    "revision": "4ba2ce8afd05695772ee",
+    "url": "/fortunoff-metadash/static/js/main.767eb456.chunk.js"
+  },
+  {
+    "revision": "74ded0f82e8e1bfa3b30",
+    "url": "/fortunoff-metadash/static/js/runtime~main.0a15b005.js"
+  },
+  {
+    "revision": "ab2c7571e384b44a9239c04088a2f639",
+    "url": "/fortunoff-metadash/static/media/dropdown-arrow.ab2c7571.svg"
+  },
+  {
+    "revision": "0d20b8076b76513b134aea5ba1ac597e",
+    "url": "/fortunoff-metadash/static/media/search-icon.0d20b807.svg"
+  },
+  {
+    "revision": "05b91349df87902b2ab9534135ac7aa8",
+    "url": "/fortunoff-metadash/static/media/x-circle-icon.05b91349.svg"
+  },
+  {
+    "revision": "630658872973a91efd5ae4b465f17e24",
+    "url": "/fortunoff-metadash/static/media/x-field.63065887.svg"
+  },
+  {
+    "revision": "5ebd1ce20b274a1c3f5227b4abfdf585",
+    "url": "/fortunoff-metadash/static/media/x-tag-icon.5ebd1ce2.svg"
+  },
+  {
+    "revision": "470d0275c316b4e7700106e2f06687de",
+    "url": "/fortunoff-metadash/static/media/yalenew-roman-webfont.470d0275.woff"
+  },
+  {
+    "revision": "f2b260ad19889584280d4dfa48cde97f",
+    "url": "/fortunoff-metadash/static/media/yalenew-roman-webfont.f2b260ad.woff2"
+  }
+]);
